b0bc4dfeb1cd0e0d18107cee10d843d81e8a927c
MarcelloBB <marcellobelanda@gmail.com>
2025-04-10 18:00:42 -0300

    feat: add redis cache

--- usecase/product_usecase.go
+++ usecase/product_usecase.go
@@ -1,6 +1,10 @@
 package usecase
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/MarcelloBB/gin-boilerplate/db"
 	"github.com/MarcelloBB/gin-boilerplate/model"
 	"github.com/MarcelloBB/gin-boilerplate/repository"
 )
@@ -16,9 +20,32 @@ func NewProductUseCase(repo repository.ProductRepository) ProductUseCase {
 }
 
 func (p *ProductUseCase) GetProducts() ([]model.Product, error) {
-	return p.repository.GetProducts()
-	// if err != nil {
-	// 	return []model.Product{}, err
-	// }
-	// return products, nil
+	cacheValue, err := db.GetCacheValue(db.Ctx, "products")
+	if err == nil && cacheValue != "" {
+		var productsResult []model.Product
+		err = json.Unmarshal([]byte(cacheValue), &productsResult)
+
+		if err != nil {
+			fmt.Println("Error unmarshalling cache:", err)
+		} else {
+			return productsResult, nil
+		}
+	}
+
+	products, err := p.repository.GetProducts()
+	if err != nil {
+		fmt.Println("Error fetching products:", err)
+		return []model.Product{}, err
+	}
+
+	productsSerialized, err := json.Marshal(products)
+	if err != nil {
+		fmt.Println("Error serializing products:", err)
+	} else {
+		if err := db.SetCacheValue(db.Ctx, "products", productsSerialized); err != nil {
+			fmt.Printf("Error setting cache: %v\n", err)
+		}
+	}
+
+	return products, nil
 }
